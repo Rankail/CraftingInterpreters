40d967d21b5b2bceb7f28699f7674022f0ce3ab1
Samuel Thesing <thesing@se-rwth.de>
2022-12-19 19:32:54 +0100

    clox scanning on demand partial

--- clox/src/debug.c
+++ clox/src/debug.c
@@ -2,8 +2,7 @@
 #include "debug.h"
 #include "value.h"
 
-void disassembleChunk(Chunk* chunk, const char* name)
-{
+void disassembleChunk(Chunk* chunk, const char* name) {
     printf("== %s ==\n", name);
 
     for (int offset = 0; offset < chunk->count;) {
@@ -11,14 +10,12 @@ void disassembleChunk(Chunk* chunk, const char* name)
     }
 }
 
-static int simpleInstruction(const char* name, int offset)
-{
+static int simpleInstruction(const char* name, int offset) {
     printf("%s\n", name);
     return offset + 1;
 }
 
-static int constantInstruction(const char* name, Chunk* chunk, int offset)
-{
+static int constantInstruction(const char* name, Chunk* chunk, int offset) {
     uint8_t constant = chunk->code[offset + 1];
     printf("%-16s %4d '", name, constant);
     printValue(chunk->constants.values[constant]);
@@ -26,23 +23,28 @@ static int constantInstruction(const char* name, Chunk* chunk, int offset)
     return offset + 2;
 }
 
-int disassembleInstruction(Chunk* chunk, int offset)
-{
+int disassembleInstruction(Chunk* chunk, int offset) {
     printf("%04d ", offset);
-    if (offset > 0 && chunk->lines[offset] == chunk->lines[offset - 1])
-    {
+    if (offset > 0 && chunk->lines[offset] == chunk->lines[offset - 1]) {
         printf("   | ");
-    }
-    else
-    {
+    } else {
         printf("%4d ", chunk->lines[offset]);
     }
 
     uint8_t instruction = chunk->code[offset];
-    switch (instruction)
-    {
+    switch (instruction) {
     case OP_CONSTANT:
         return constantInstruction("OP_CONSTANT", chunk, offset);
+    case OP_ADD:
+        return simpleInstruction("OP_ADD", offset);
+    case OP_SUBTRACT:
+        return simpleInstruction("OP_SUBTRACT", offset);
+    case OP_MULTIPLY:
+        return simpleInstruction("OP_MULTIPLY", offset);
+    case OP_DIVIDE:
+        return simpleInstruction("OP_DIVIDE", offset);
+    case OP_NEGATE:
+        return simpleInstruction("OP_NEGATE", offset);
     case OP_RETURN:
         return simpleInstruction("OP_RETURN", offset);
     default:
