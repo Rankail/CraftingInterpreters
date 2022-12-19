83976b402c4c6bbfa1ec15bd63919b4c920c9458
Rankail <thes.samue@gmail.com>
2022-12-19 23:19:17 +0100

    clox scanning on demand

--- clox/src/vm.c
+++ clox/src/vm.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "common.h"
+#include "compiler.h"
 #include "debug.h"
 #include "vm.h"
 
@@ -28,9 +29,9 @@ Value pop() {
 }
 
 static InterpretResult run() {
-    #define READ_BYTE() (*vm.ip++)
-    #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
-    #define BINARY_OP(op) \
+#define READ_BYTE() (*vm.ip++)
+#define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+#define BINARY_OP(op) \
         do { \
             double b = pop(); \
             double a = pop(); \
@@ -55,10 +56,10 @@ static InterpretResult run() {
             push(constant);
             break;
         }
-        case OP_ADD     : BINARY_OP(+); break;
+        case OP_ADD: BINARY_OP(+); break;
         case OP_SUBTRACT: BINARY_OP(-); break;
         case OP_MULTIPLY: BINARY_OP(*); break;
-        case OP_DIVIDE  : BINARY_OP(/); break;
+        case OP_DIVIDE: BINARY_OP(/ ); break;
         case OP_NEGATE:     push(-pop()); break;
         case OP_RETURN: {
 
@@ -68,13 +69,12 @@ static InterpretResult run() {
         }
         }
     }
-    #undef READ_BYTE
-    #undef READ_CONSTANT
-    #undef BINARY_OP
+#undef READ_BYTE
+#undef READ_CONSTANT
+#undef BINARY_OP
 }
 
-InterpretResult interpret(Chunk* chunk) {
-    vm.chunk = chunk;
-    vm.ip = vm.chunk->code;
-    return run();
+InterpretResult interpret(const char* source) {
+    compile(source);
+    return INTERPRET_OK;
 }
